6eeed4971edd15934e1bede95490d65d1c078bf0
Sara Gozalo <sara89sgm@gmail.com>
2013-12-07 18:32:46 +0000

    adding track

--- XmasHackCloud/cloud/main.js
+++ XmasHackCloud/cloud/main.js
@@ -1,33 +1,50 @@
-var remixer = require('cloud/remix.js');
 var twilio = require('twilio')('AC8183497cebb16c724f8a12bf235467bf', '56b4b4677822b38484de1112b15079d5');
-var twiliolib = require('twilio')
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("destroyCarol", function(request, response) {
-
-	var Track = Parse.Object.extend("track");
-	var queryXmas = new Parse.Query(Track);
-	queryXmas.equalTo("type", "xmas");
-	queryXmas.first({
-		success: function(xmasTrack){
-			var queryPop = new Parse.Query(Track);
-			queryPop.notEqualTo("type", "xmas");
-			queryPop.first({
-				success: function(popTrack){
-					mixTracks(xmasTrack, popTrack);
-				}
-			})
-		}
-	})
-  
-});
 
 Parse.Cloud.define("urlSong", function(request, response){
-	var trackUrl = 'http://xmashackmhd13.parseapp.com/eminem-the-monster-feat.mp3';
-  	var twiml = new twiliolib.TwimlResponse();
-  	console.log("url song called");
-  	response.success(twiml.play(trackUrl).toString());
+
+	var Track = Parse.Object.extend("track");
+	var queryXmas = new Parse.Query(Track),
+		xmas_trid = '',
+		xmas_file_name = '',
+		other_trid = '',
+		other_file_name ='';
+	queryXmas.find({
+		success: function(results){
+			for (var i = 0; i < results.length; i++) { 
+		      	var object = results[i];
+		      	if(object.get('type')==='xmas'){
+		      		xmas_trid = object.get('ec_trid');
+		      		xmas_file_name = object.get('url_id');
+		      	}else{
+		      		other_trid = object.get('ec_trid');
+		      		other_file_name = object.get('url_id');
+		      	}
+		    }
+			Parse.Cloud.httpRequest({
+				url: 'http://54.217.227.133:8080/test',
+				params: {
+		    		xmas_id : xmas_trid,
+		    		xmas_file : xmas_file_name,
+		    		other_id : other_trid,
+		    		other_file : other_file_name
+		  		},
+
+				success: function(httpResponse) {
+					var trackUrl = 'http://xmashackmhd13.parseapp.com/eminem-the-monster-feat.mp3';
+		  			var twiml = new twiliolib.TwimlResponse();
+		  			console.log("url song called");
+		  			response.success(twiml.play(trackUrl).toString());
+					
+				},
+				error: function(httpResponse) {
+				    def.resolve("Error");
+				}
+			});
+		}
+	});
+
+	
+	
 });
 
 Parse.Cloud.define("receiveSMS", function(request, response) {
