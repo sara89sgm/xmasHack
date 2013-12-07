12c453c86439d6ef458f9dbd9bea401b56abb3f0
Sara Gozalo <sara89sgm@gmail.com>
2013-12-07 14:43:13 +0000

    twilio

--- XmasHackCloud/cloud/main.js
+++ XmasHackCloud/cloud/main.js
@@ -1,5 +1,5 @@
 var remixer = require('cloud/remix.js');
-var twilio = require('twilio');
+var twilio = require('twilio')('AC6fce2fb8441b4279897882f82ad80ed3', 'c1805ed5bd9fc3a8d17a200688421600');
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
@@ -38,6 +38,22 @@ Parse.Cloud.define("destroyCarol", function(request, response) {
 
 Parse.Cloud.define("receiveSMS", function(request, response) {
   console.log("Received a new text: " + request.params.From);
+  var client = require('twilio')
+ 
+	// Send an SMS message
+	client.sendSms({
+	    to: request.params.From, 
+	    from: '+441753463246', 
+	    body: 'Hello world!'
+	  }, function(err, responseData) { 
+	    if (err) {
+	      console.log(err);
+	    } else { 
+	      console.log(responseData.from); 
+	      console.log(responseData.body);
+	    }
+	  }
+	);
   response.success();
 });
 
